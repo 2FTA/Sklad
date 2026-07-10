df6676c9aab698b86c7fd4bb941daaf303ca2659
2FTA <7196768.mm@gmail.com>
2026-07-10 17:31:01 +0300

    Обновлен адрес сервера на Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://sklad-iyib.onrender.com',
         changeOrigin: true,
       },
     },
